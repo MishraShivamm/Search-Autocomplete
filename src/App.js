575bdf94d701eff7426c0ed86d03206312f76a0b
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-06-03 09:11:01 +0000

    Autocomplete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React,{Component} from 'react';
 import AutoCompletedText from './CountrySearch';
 import './App.css';
 
+class App extends Component{
+  render(){
+    return (
+      <div ClassName="App">
+        <AutoCompletedText/>
 
+      </div>
+    )
+  }
+
+
+}
 
 export default App;
 
